bb2e19726e46023ca23ff9ec938b3416f86004ef
Quelvinmp <quelvinmp@gmail.com>
2023-03-13 16:33:18 -0300

    resolve requisito 2

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,68 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
+import { createUser } from '../services/userAPI';
+import Loading from './Loading';
 
 class Login extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      inputLogin: '',
+      isDisabled: true,
+      buttonClicked: false,
+      result: '',
+    };
+  }
+
+  enableButton = (inputLogin) => {
+    const minimalChar = 3;
+    if (inputLogin.length >= minimalChar) {
+      this.setState({ isDisabled: false });
+    } else {
+      this.setState({ isDisabled: true });
+    }
+  };
+
+  handleInputChange = ({ target: { value } }) => {
+    this.setState({ inputLogin: value });
+    this.enableButton(value);
+  };
+
+  onClickButton = async () => {
+    const { inputLogin } = this.state;
+    this.setState({ buttonClicked: true });
+    const result = await createUser({ name: inputLogin });
+    this.setState({ result });
+  };
+
   render() {
+    const { isDisabled, inputLogin, buttonClicked, result } = this.state;
     return (
       <div data-testid="page-login">
-        <h1>oi</h1>
+        <form>
+          <label htmlFor="login">
+            <input
+              type="text"
+              name="login"
+              id="login"
+              data-testid="login-name-input"
+              value={ inputLogin }
+              onChange={ this.handleInputChange }
+            />
+          </label>
+          <button
+            type="button"
+            data-testid="login-submit-button"
+            disabled={ isDisabled }
+            onClick={ this.onClickButton }
+          >
+            Entrar
+
+          </button>
+          { buttonClicked && <Loading />}
+          { result === 'OK' && <Redirect to="/search" />}
+        </form>
       </div>
     );
   }
